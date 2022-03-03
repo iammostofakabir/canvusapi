b983ae5ccf0c6323620eecb9ba994c6bd15b58fb
unknown <mksunnybd@gmail.com>
2022-03-03 20:19:24 +0600

    aa

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var cors = require('cors');
+
+app.use(cors());
 
 io.on('connection', (socket)=>{
     console.log('User Online');
